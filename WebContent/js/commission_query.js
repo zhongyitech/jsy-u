0fac4d584c879bc5cc6afe9595b2711ed4de81b3
libosong1986,./ <bosong123@qq.com>
2015-03-31 15:23:08 +0800

    unchange

--- WebContent/js/commission_query.js
+++ WebContent/js/commission_query.js
@@ -220,7 +220,7 @@ var VIEWDATA = {
         });
     },
     success: function(result){
-        this.items = JSON.parse(result['rest_result']);
+        this.items = result['rest_result'];//JSON.parse(result['rest_result']);
         this.setTable(this.items);
         this.setPage(result);
     },
@@ -433,3 +433,71 @@ var COMMISSION_REPORT = {
         }
     }
 };
+
+(function($){
+    var Util={
+        _entity:{
+            startposition: 0,
+            pagesize: 10,
+            type:"or",
+            fields:["czr","url","method","params","address"],
+            value:"",
+            order:{id:"desc"}
+        },
+        /**
+         * 发送请求，接收Options参数可包含entity、params
+         * @param options
+         * @returns {*}
+         * @private
+         */
+        _request:function(options){
+            $.extend(true,this._entity,options.entity)
+            return $.io.post({
+                url: '/api/operationRecord/readAllForPage',
+                entity:this._entity
+            });
+        },
+        /**
+         * 渲染方法，当前页面只接收entity参数
+         * @param entity
+         * @private
+         */
+        _render:function(entity){
+            var _this=this;
+            _this._request({entity:entity}).success(function(result,pager){
+                _this._renderData(result);
+                _this._renderPage(pager);
+            });
+        },
+        _renderData:function(result){
+            var _this=this;
+            $("#table-data").renderData("#table-data-template",result,function(){
+                return _this._entity.startposition;
+            });
+        },
+        _renderPage:function(pager){
+            var _this=this;
+            if(!_this._entity.startposition){
+                $.dom.pager("#table-pager",pager,{
+                    pageSize:_this._entity.pagesize
+                }).onChange(function(entity){
+                    _this._render(entity);
+                });
+            }
+        },
+        _bindEvent:function(){
+            var _this=this,keyword=$("#keyword-input"),searchButton=$("#keyword-button");
+            keyword.unbind("keyup").bind("keyup",function(e){
+                if(e.keyCode==13) _this._render({startposition: 0,value:keyword.val()});
+            });
+            searchButton.unbind("click").bind("click",function(){
+                _this._render({startposition: 0,value:keyword.val()});
+            });
+        },
+        render:function(){
+            this._render();
+            this._bindEvent();
+        }
+    };
+    Util.render();
+})(jQuery);
