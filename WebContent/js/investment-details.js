a3629e077050d4541c500206d4fb987dbd2e3531
liujw <P2ssw0rd>
2015-04-08 18:41:27 +0800

    查看投资档案 明细

--- WebContent/js/investment-details.js
+++ WebContent/js/investment-details.js
@@ -1,7 +1,7 @@
-(function($){
-    var Util={
-        _params:{
-            id:18
+(function ($) {
+    var Util = {
+        _params: {
+            id: 0
         },
         /**
          * 发送请求，接收Options参数可包含entity、params
@@ -9,54 +9,61 @@
          * @returns {*}
          * @private
          */
-        _request:function(){
+        _request: function () {
             //$.extend(true,this._entity,options.entity)
             return $.io.get({
                 url: '/api/investmentArchives/detail',
-                params:this._params
+                params: this._params
             });
         },
-        _getUserName:function(id){
-            return $.project.domain(id,"com.jsy.auth.User","chainName").getItem(id).chainName;
+        _getUserName: function (id) {
+            return $.project.domain(id, "com.jsy.auth.User", "chainName").getItem(id).chainName;
         },
         /**
          * 渲染方法，当前页面只接收entity参数
          * @param entity
          * @private
          */
-        _render:function(entity){
-            var _this=this;
-            _this._request({entity:entity}).success(function(result,pager){
-                _this._renderData(result);
-                //_this._renderPage(pager);
+        _render: function (entity) {
+            var _this = this;
+
+            var id =PAGE.getParam("id");
+            if (id) {
+                _this._params={id:id};
+                _this._request({entity: entity}).success(function (result, pager) {
+                    _this._renderData(result);
+                    //_this._renderPage(pager);
+                });
+            }
+        },
+        _renderData: function (result) {
+            var _this = this;
+            $("#page-content").renderData("#info-data-template", result, {
+                callback: function () {
+                    return _this._entity.startposition;
+                }, util: _this
             });
         },
-        _renderData:function(result){
-            var _this=this;
-            $("#page-content").renderData("#info-data-template",result,{callback:function(){
-                return _this._entity.startposition;
-            },util:_this});
-        },
-        _renderPage:function(pager){
-            var _this=this;
-            if(!_this._entity.startposition){
-                $.dom.pager("#table-pager",pager,{
-                    pageSize:_this._entity.pagesize
-                }).onChange(function(entity){
+        _renderPage: function (pager) {
+            var _this = this;
+            if (!_this._entity.startposition) {
+                $.dom.pager("#table-pager", pager, {
+                    pageSize: _this._entity.pagesize
+                }).onChange(function (entity) {
                     _this._render(entity);
                 });
             }
         },
-        _bindEvent:function(){
-            var _this=this,keyword=$("#keyword-input"),searchButton=$("#keyword-button");
-            keyword.unbind("keyup").bind("keyup",function(e){
-                if(e.keyCode==13) _this._render({startposition: 0,value:keyword.val()});
+        _bindEvent: function () {
+            var _this = this, keyword = $("#keyword-input"), searchButton = $("#keyword-button");
+            keyword.unbind("keyup").bind("keyup", function (e) {
+                if (e.keyCode == 13) _this._render({startposition: 0, value: keyword.val()});
             });
-            searchButton.unbind("click").bind("click",function(){
-                _this._render({startposition: 0,value:keyword.val()});
+            searchButton.unbind("click").bind("click", function () {
+                _this._render({startposition: 0, value: keyword.val()});
             });
         },
-        render:function(){
+        render: function () {
             this._render();
             this._bindEvent();
         }
