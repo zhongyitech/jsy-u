62a5b0bd9342acb5dee239d8192f9e0acfc4fa40
lioa <lisz@zhongyitech.com>
2015-04-13 19:12:29 +0800

    凭证生成设置

---
+++ WebContent/js/bankordersetting.js
@@ -0,0 +1,348 @@
+(function ($) {
+
+    var UtilSummary = {
+        _entity: {
+            startposition: 0,
+            pagesize: 5,
+            type: "or",
+            fields: ["name"],
+            value: "",
+            order: {indexlocation: "asc"}
+        },
+        _items: [],
+        _request: function (options) {
+            $.extend(true, this._entity, options.entity)
+            return $.io.post({
+                url: '/api/summary/readAllForPage',
+                entity: this._entity
+            });
+        },
+        _render: function (entity) {
+            var _this = this;
+            _this._request({entity: entity}).success(function (result, pager) {
+                _this._renderData(result);
+                _this._renderPage(pager);
+            });
+        },
+        _renderData: function (result) {
+            var _this = this;
+            $("#banktosummary-data").renderData("#banktosummary-data-template", result, function () {
+                return _this._entity.startposition;
+            });
+            _this._items = result;
+
+            //$("#bank-table button[class=del-btn]").unbind().bind('click', function () {
+            //    _this.delItem('', function () {
+            //        $(this).closest('tr').remove();
+            //        _this._render();
+            //    });
+            //});
+            $("#banktosummary-new").unbind().bind('click', function () {
+                $('#banktosummary-table').append(
+                    '<tr data-key="0"><td class="form-input"><input type="hidden" name="id" value="-1"><input name="summary" value=""></td>' +
+                    ' <td class="form-input"><input name="remark" value=""></td><td class="form-input"><input name="accountName" value=""></td>' +
+                    '<td class="form-input"><label></label></td><td><button class="del-btn" data-itemid="-1">删除</button>' +
+                    '<button class="up-btn" data-itemid="-1">上移</button><button class="down-btn" data-itemid="-1">下移</button></td></tr>'
+                );
+                _this.bindButtonClicK();
+            });
+            _this.bindButtonClicK();
+            $("#banktosummary-save").unbind().bind('click', function () {
+                _this._submit();
+            })
+        },
+        bindButtonClicK: function () {
+            var _this = this;
+            $("#banktosummary-table button[class=del-btn]").unbind().bind('click', function () {
+                var key = $(this).data("itemid");
+                var _target = this;
+                _this.delItem(key, function () {
+                    //$(_target).closest('tr').remove();
+                    _this.render();
+                });
+            });
+        },
+        delItem: function (key, callFunc) {
+            var _this = this;
+            if (key == undefined || key == null || _this._items[key] == undefined) {
+                if (callFunc != null) {
+                    callFunc();
+                }
+                return;
+            }
+            //todo:edit url
+            var data = {url: '/api/summary', params: {id: _this._items[key].id}};
+            $.io.del(data).success(function (result) {
+                if (callFunc != null) {
+                    callFunc();
+                }
+            }).error(function (error) {
+                alert(error.msg);
+            })
+        },
+        _submit: function () {
+            var items = [];
+            $('#banktosummary-table tr:gt(0)').each(function (i, tr) {
+                var item = {};
+                $(tr).find('input').each(function (i, obj) {
+                    item[$(obj).prop('name')] = $(obj).val();
+                });
+                items.push(item);
+            });
+            console.log(items);
+            //todo:submit
+            $(items).each(function (i, item) {
+                if (item.id < 0) {
+                    //add
+                    $.io.post(true, {url: '/api/summary', entity: item})
+                        .success(function (result) {
+                            $.message.log('数据保存成功:' + item.summary);
+                        })
+                        .error(function (error) {
+                            $.message.log(error.msg);
+                        });
+                } else {
+                    //update
+                    $.io.put(true, {url: '/api/summary', entity: item, params: {id: item.id}})
+                        .success(function (result) {
+                            $.message.log('数据修改成功:' + item.summary);
+                        })
+                        .error(function (error) {
+                            $.message.log(error.msg);
+                        });
+                }
+            });
+            this.render();
+        },
+        _saveUserInfo: function () {
+            var obj = {};
+            var _this = this;
+            $('#userinfo input,select,textarea').each(function (i, item) {
+                var key = $(item).prop('name');
+                if (key == "") {
+                    return;
+                }
+                var value = null;
+                if (item.tagName == 'INPUT' || item.tagName == 'SELECT' || item.tagName == 'TEXTAREA') {
+                    value = $(item).val();
+                }
+                obj[key] = value;
+            });
+            var bankAccount = [];
+            $('#bank-table tr:gt(0)').each(function (i, item) {
+                var account = {};
+                $(item).find('input').each(function (i, item) {
+                    if ($(item).prop('name') == 'defaultAccount') {
+                        account["defaultAccount"] = $(item).prop('checked');
+                    } else {
+                        account[$(item).prop('name')] = $(item).val();
+                    }
+                });
+                bankAccount.push(account);
+            });
+            obj['bankAccount'] = bankAccount;
+            var data = {};
+            if ($('#customerId').val() > 0) {
+                //updaate
+                var data = {url: '/api/customerArchives/update', entity: obj, params: {id: $('#customerId').val()}};
+                $.io.post(data).success(function (result) {
+                    $('#userinfo').fadeOut(100);
+                    _this.render();
+                    $.message.log("修改成功");
+                    //alert("!");
+                })
+                    .error(function (error) {
+                        alert(error.msg);
+                    });
+            } else {
+                //create
+                var data = {url: '/api/customerArchives', entity: obj};
+                $.io.put(data).success(function (result) {
+                    //alert("保存成功!");
+                    //window.location.reload();
+                    $('#userinfo').fadeOut(100);
+                    _this.render();
+                    $.message.log("新建客户成功!");
+                })
+                    .error(function (error) {
+                        alert(error.msg);
+                    });
+            }
+        }
+        ,
+        _renderPage: function (pager) {
+            var _this = this;
+            if (!_this._entity.startposition) {
+                $.dom.pager("#banktosummary-pager", pager, {
+                    pageSize: _this._entity.pagesize
+                }).onChange(function (entity) {
+                    _this._render(entity);
+                });
+            }
+        }
+        ,
+        _bindEvent: function () {
+            var _this = this, keyword = $("#keyword-input"), searchButton = $("#keyword-button");
+            keyword.unbind("keyup").bind("keyup", function (e) {
+                if (e.keyCode == 13) _this._render({startposition: 0, value: keyword.val()});
+            });
+            searchButton.unbind("click").bind("click", function () {
+                _this._render({startposition: 0, value: keyword.val()});
+            });
+        }
+        ,
+        render: function () {
+            this._render();
+            this._bindEvent();
+        }
+    };
+    UtilSummary.render();
+})
+(jQuery);
+
+(function ($) {
+
+    var UtilSubject = {
+        _entity: {
+            startposition: 0,
+            pagesize: 5,
+            type: "or",
+            fields: ["name"],
+            value: "",
+            order: {id: "asc"}
+        },
+        _items: [],
+        _request: function (options) {
+            $.extend(true, this._entity, options.entity)
+            return $.io.post({
+                url: '/api/summaryToFund/readAllForPage',
+                entity: this._entity
+            });
+        },
+        _render: function (entity) {
+            var _this = this;
+            _this._request({entity: entity}).success(function (result, pager) {
+                _this._renderData(result);
+                _this._renderPage(pager);
+            });
+        },
+        _renderData: function (result) {
+            var _this = this;
+            $("#subject-data").renderData("#subject-data-template", result, {
+                callback: function () {
+                    return _this._entity.startposition;
+                }, selectlist: []
+            });
+            _this._items = result;
+
+            $("#subject-new").unbind().bind('click', function () {
+                $('#subject-table').append(
+                    '<tr >                ' +
+                    '<td class="form-input"><input type="hidden" name="id" value="-1"><input name="subject" value="">  </td>' +
+                    '<td class="form-input"><input name="subjectLevel2" value=""></td>   ' +
+                    '<td class="form-input"><select name="borrowAndLend"><option value="true">借</option><option value="false" >贷</option>  ' +
+                    '</select></td><td class="form-input"><input name="sumName" value=""></td>' +
+                    '<td class="form-input"><input name="fund" value=""></td> ' +
+                    '<td> <button class="del-btn" data-itemid="0">删除</button></td> ' +
+                    '</tr>'
+                );
+                _this.bindButtonClicK();
+            });
+            _this.bindButtonClicK();
+            $("#subject-save").unbind().bind('click', function () {
+                _this._submit();
+            })
+
+        },
+        bindButtonClicK: function () {
+            var _this = this;
+            $("#subject-table button[class=del-btn]").unbind().bind('click', function () {
+                var key = $(this).data("itemid");
+                var _target = this;
+                _this.delItem(key, function () {
+                    //$(_target).closest('tr').remove();
+                    _this.render();
+                });
+            });
+        },
+        delItem: function (key, callFunc) {
+            var _this = this;
+            if (key == undefined || key == null || _this._items[key] == undefined) {
+                if (callFunc != null) {
+                    callFunc();
+                }
+                return;
+            }
+            //todo:edit url
+            var data = {url: '/api/summaryToFund', params: {id: _this._items[key].id}};
+            $.io.del(data).success(function (result) {
+                if (callFunc != null) {
+                    callFunc();
+                }
+            }).error(function (error) {
+                alert(error.msg);
+            })
+        },
+        _submit: function () {
+            var items = [];
+            $('#subject-table tr:gt(0)').each(function (i, tr) {
+                var item = {};
+                $(tr).find('input,select').each(function (i, obj) {
+                    item[$(obj).prop('name')] = $(obj).val();
+                });
+                items.push(item);
+            });
+            console.log(items);
+            //todo:submit
+            $(items).each(function (i, item) {
+                if (item.id < 0) {
+                    //add
+                    $.io.post(true, {url: '/api/summaryToFund', entity: item})
+                        .success(function (result) {
+                            $.message.log('数据保存成功:' );
+                        })
+                        .error(function (error) {
+                            $.message.log(error.msg);
+                        });
+                } else {
+                    //update
+                    $.io.put(true, {url: '/api/summaryToFund', entity: item, params: {id: item.id}})
+                        .success(function (result) {
+                            $.message.log('数据修改成功:' + item.subject);
+                        })
+                        .error(function (error) {
+                            $.message.log(error.msg);
+                        });
+                }
+            });
+            this.render();
+        },
+
+        _renderPage: function (pager) {
+            var _this = this;
+            if (!_this._entity.startposition) {
+                $.dom.pager("#subject-pager", pager, {
+                    pageSize: _this._entity.pagesize
+                }).onChange(function (entity) {
+                    _this._render(entity);
+                });
+            }
+        }
+        ,
+        _bindEvent: function () {
+            var _this = this, keyword = $("#keyword-input"), searchButton = $("#keyword-button");
+            keyword.unbind("keyup").bind("keyup", function (e) {
+                if (e.keyCode == 13) _this._render({startposition: 0, value: keyword.val()});
+            });
+            searchButton.unbind("click").bind("click", function () {
+                _this._render({startposition: 0, value: keyword.val()});
+            });
+        }
+        ,
+        render: function () {
+            this._render();
+            this._bindEvent();
+        }
+    };
+    UtilSubject.render();
+})(jQuery);
---
+++ WebContent/view/bankordersetting.jsp
@@ -0,0 +1,163 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <jsp:include page="./head.jsp"/>
+    <link rel="stylesheet" type="text/css" href="/css/company-edit.css">
+    <title>记账凭证生成设置</title>
+</head>
+<body>
+<div id="page-wrapper">
+    <jsp:include page="./navi.jsp"/>
+    <textarea id="banktosummary-data-template" class="template">
+        <table class="table table-striped text-center mrg0B" id="banktosummary-table">
+            <thead>
+            <tr>
+                <%--根据客户权限多加一列操作列--%>
+                <th class="text-center">摘要名称</th>
+                <th class="text-center">关键词</th>
+                <th class="text-center">银行账号</th>
+                <th class="text-center">匹配顺序</th>
+                <th class="text-center">操作</th>
+            </tr>
+            </thead>
+            <tbody>
+            {#foreach $T as item}
+            <tr data-key="{$T.item$index}">
+                <td class="form-input"><input type="hidden" name="id" value="{$T.item['id']}"><span><input
+                        name="summary" value="{$T.item['summary']}"/></span>
+                </td>
+                <td class="form-input"><input name="remark" value="{$T.item['remark']}"/></td>
+                <td class="form-input"><input name="accountName" value="{$T.item['accountName']}"></td>
+                <td class="form-input"><label>{$T.item['indexlocation']}</label></td>
+                <td>
+                    <button class="del-btn" data-itemid="{$T.item$index}">删除</button>
+                    <button class="up-btn" data-itemid="{$T.item$index}">上移</button>
+                    <button class="down-btn" data-itemid="{$T.item$index}">下移</button>
+                </td>
+            </tr>
+            {#/for}
+            </tbody>
+        </table>
+    </textarea>
+    <textarea id="subject-data-template" class="template">
+        <table class="table table-striped text-center mrg0B" id="subject-table">
+            <thead>
+            <tr>
+                <%--根据客户权限多加一列操作列--%>
+                <th class="text-center">一级科目名称</th>
+                <th class="text-center">二级科目名称</th>
+                <th class="text-center">借贷方式</th>
+                <th class="text-center">摘要</th>
+                <th class="text-center">账套</th>
+                <th class="text-center" width="150px"></th>
+            </tr>
+            </thead>
+            <tbody>
+            {#foreach $T as item}
+            <tr data-key="{$T.item$index}">
+                <td class="form-input"><input type="hidden" name="id" value="{$T.item['id']}"><input name="subject"
+                                                                                                     value="{$T.item['subject']}"/>
+                </td>
+                <td class="form-input"><input name="subjectLevel2" value="{$T.item['subjectLevel2']}"/></td>
+                <td class="form-input"><select name="borrowAndLend">
+                    <option value="true" {$T.item[
+                    'borrowAndLend'] ? 'selected' : ''}>借</option>
+                    <option value="false" {$T.item[
+                    'borrowAndLend'] ? '' : 'selected'}>贷</option>
+                </select></td>
+                <td class="form-input">
+                    <%--<select>--%>
+                    <%--<option value="{$T.item['sumName']}" >{$T.item['sumName']}</option>--%>
+                    <%--</select>--%>
+                    <input name="sumName" value="{$T.item['sumName']}"/>
+                </td>
+                <td class="form-input"><input name="fund" value="{$T.item['fund']}"/></td>
+                <%--<td><span ><input name="defaultAccount" value="{$T.item['defaultAccount']}" /></span></td>--%>
+                <td><span class="text-overflow">
+                    <button class="del-btn" data-itemid="{$T.item$index}">删除</button>
+                </span></td>
+            </tr>
+            {#/for}
+            </tbody>
+        </table>
+    </textarea>
+
+    <div id="page-content-wrapper">
+        <div id="page-content" class="page-view pad25T">
+            <div class="content-box box-toggle " id="company-view">
+                <div class="content-box-header primary-bg">
+                    <span class="float-left">银行备注-摘要匹配数据设置</span>
+                    <a href="#" class="float-right icon-separator btn toggle-button" title="点击">
+                        <i class="glyph-icon icon-toggle icon-chevron-down"></i>
+                    </a>
+                </div>
+                <div class="content-box-wrapper">
+                    <div id="banktosummary-pager" class="page-bar"></div>
+                    <div class="table table-striped text-center mrg0B" id="banktosummary-data">
+                    </div>
+                    <div class="form-row mrg10T">
+                        <div class="form-input col-md-6">
+                            <button class="btn medium bg-primary" title="" id="banktosummary-new">
+                                <span class="button-content">+ 新行</span>
+                            </button>
+                        </div>
+                        <div class="form-input col-md-6">
+                            <button class="btn medium bg-green float-right mrg10L" title="" id="banktosummary-save">
+                                <span class="button-content">保存</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content-box box-toggle " id="">
+                <div class="content-box-header primary-bg">
+                    <span class="float-left">科目匹配设置</span>
+                    <a href="#" class="float-right icon-separator btn toggle-button" title="点击">
+                        <i class="glyph-icon icon-toggle icon-chevron-down"></i>
+                    </a>
+                </div>
+                <div class="content-box-wrapper">
+                    <div id="subject-pager" class="page-bar"></div>
+                    <div class="table table-striped text-center mrg0B" id="subject-data">
+                    </div>
+                    <div class="form-row mrg10T">
+                        <div class="form-input col-md-6">
+                            <button class="btn medium bg-primary" title="" id="subject-new">
+                                <span class="button-content">+新行</span>
+                            </button>
+                        </div>
+                        <div class="form-input col-md-6">
+                            <button class="btn medium bg-green float-right mrg10L" title="" id="subject-save">
+                                <span class="button-content" >保存</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content-box box-toggle ">
+                <div class="content-box-header primary-bg">
+                    <span class="float-left">凭证生成参数设置</span>
+                    <a href="#" class="float-right icon-separator btn toggle-button" title="点击">
+                        <i class="glyph-icon icon-toggle icon-chevron-down"></i>
+                    </a>
+                </div>
+                <div class="content-box-wrapper">
+                    <div id="setting-pager" class="page-bar"></div>
+                    <div class="table table-striped text-center mrg0B" id="setting-data">
+                    </div>
+                    <div CLASS="mrg10T">
+                        <div class="button-pane">
+                            <button class="btn-ui btn bg-green large medium float-right mrg10L" id="save-button"><span
+                                    class="button-content">保存设置</span></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<script type="text/javascript" src="/js/bankordersetting.js"></script>
+</body>
+</html>
